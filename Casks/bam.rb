b116fc1ba920484e891f5886f91a4087b13934e6
lkshrk <dev@harke.me>
2026-05-30 20:42:06 +0200

    bam: bump to 0.5.0

---
+++ Casks/bam.rb
@@ -0,0 +1,18 @@
+cask "bam" do
+  version "0.5.0"
+  sha256 "f6910c4fe289c7842429a707086056c0657108ea50c5c836807424e65cfe57e8"
+
+  url "https://github.com/lkshrk/better-audio-mixer/releases/download/v#{version}/bam.zip"
+  name "bam"
+  desc "Per-app audio mixer and router for macOS"
+  homepage "https://github.com/lkshrk/better-audio-mixer"
+
+  depends_on macos: ">= :sonoma"
+
+  app "bam.app"
+
+  zap trash: [
+    "~/Library/Preferences/me.harke.bam.plist",
+    "~/Library/Application Support/bam",
+  ]
+end
