6689fb6da66f4140f8178e1cbcd547bc8cc391ed
lkshrk <dev@harke.me>
2026-08-29 12:00:36 +0200

    chore(formula): roll back omni to v0.9.32

--- Formula/omni.rb
+++ Formula/omni.rb
@@ -4,23 +4,23 @@
 class Omni < Formula
   desc "Manage all your dev tools from a single JSON config file."
   homepage "https://github.com/lkshrk/omni"
-  version "0.9.42"
+  version "0.9.32"
   license "MIT"
 
   depends_on "stow"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/lkshrk/omni/releases/download/v0.9.42/omni_darwin_x86_64.tar.gz"
-      sha256 "d796f1588999370c136578518e172f803184c2ec88af35b6497a1b49893893a4"
+      url "https://github.com/lkshrk/omni/releases/download/v0.9.32/omni_darwin_x86_64.tar.gz"
+      sha256 "c326877a357292969ad0a92e3f813466b312c681ebcc2d40a2edf8df587011cc"
 
       def install
         bin.install "omni"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/lkshrk/omni/releases/download/v0.9.42/omni_darwin_arm64.tar.gz"
-      sha256 "a97119a53cb02bace972be9e085f66f6f053675e1ca39a809eb80e7a45b27fe7"
+      url "https://github.com/lkshrk/omni/releases/download/v0.9.32/omni_darwin_arm64.tar.gz"
+      sha256 "ecabe82026088e3d37a56f828c2d8ae7cb223332837d1f1ef2a5b044ca9f30fc"
 
       def install
         bin.install "omni"
@@ -30,15 +30,15 @@ class Omni < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/lkshrk/omni/releases/download/v0.9.42/omni_linux_x86_64.tar.gz"
-      sha256 "64303e81d0315a5553c53c0bbaf31ee55ef50c45bebf3210fd6cb95d34eb0d69"
+      url "https://github.com/lkshrk/omni/releases/download/v0.9.32/omni_linux_x86_64.tar.gz"
+      sha256 "9d2369d5f73622834fb8cf7f15baf2ac5417274a9f49856cf380a8758ad8dbaf"
       def install
         bin.install "omni"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/lkshrk/omni/releases/download/v0.9.42/omni_linux_arm64.tar.gz"
-      sha256 "cced5d2be4324bb64b90f2b2610360811e5de72c2af32ccfb45c5feb46fadbe3"
+      url "https://github.com/lkshrk/omni/releases/download/v0.9.32/omni_linux_arm64.tar.gz"
+      sha256 "64c9e1997b5b5a48a0067344bb154c61d90db0b560a6cddc978ba3d3f3c441cb"
       def install
         bin.install "omni"
       end
